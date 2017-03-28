c16afa9dae98cb0b932378f9d604163e990196fd
NanCarp <nancarpli@163.com>
2017-03-28 17:59:59 +0800

    登录

--- src/mytest/controller/LoginController.java
+++ src/mytest/controller/LoginController.java
@@ -21,11 +21,20 @@ import mytest.utils.MD5Util;
 //@Before(ManageInterceptor.class)
 public class LoginController extends Controller {
 
+	/**
+	 * @description 主页面
+	 * @author liyu
+	 * @date 2017/03/28
+	 */
 	public void index() {
 		render("index.html");
 	}
 
-	// 登录页面
+	/**
+	 * @description 登录页 
+	 * @author liyu
+	 * @date 2017/03/28
+	 */
 	public void login() {
 		render("login.html");
 	}
@@ -37,14 +46,14 @@ public class LoginController extends Controller {
 	 * @author liyu
 	 * @date 2017/03/28
 	 */
-	public void adminLogin() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	public void userLogin() throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		// 登陆结果，默认false
 		boolean result = false;
 		// 登陆结果消息
-		String msg = new String();
+		String msg = new String("用户名或密码错误");
 
 		// 登录页面提交的用户名、密码
-		String username = getPara("username");
+		String username = getPara("userName");
 		String password = getPara("password");
 		// 根据用户名，从数据库查询用户记录
 		Record user = LoginService.getLoginInfo(username);
@@ -58,21 +67,17 @@ public class LoginController extends Controller {
 				msg = "登录成功";
 				// 向session存储用户信息 
 				getSession().setAttribute("user", user);
-				// 创建cookie，一段时间内免登陆
+				// 创建cookie，??
 				Cookie cookie = new Cookie("morality", "" + user.getInt("id"));
-				cookie.setMaxAge(1000 * 60 * 60 * 24 * 7);// 7天免登陆
+				cookie.setMaxAge(1000 * 60 * 60 * 24 * 7);// 7天
 				cookie.setPath("/login/");//？？
 				getResponse().addCookie(cookie);
 			}
-		} else {
-			// 用户不存在或者密码错误
-			msg = "用户名或密码错误";
-		}
-		
+		} 
 		// 结果数据写入map，以json格式传给页面
 		Map<String, Object> responseMap = new HashMap<>();
 		responseMap.put("result", result);
-		responseMap.put("msg", "登陆成功");
+		responseMap.put("msg", msg);
 		renderJson(responseMap);
 	}
 }
